ff6bc52243617c20cffc3411ba41787d2908338a
xtaieer <920557080@qq.com>
2019-05-10 09:24:29 +0800

    添加文件

---
+++ EchoClient/src/main/java/xtaieer/network/echo/EchoClient.java
@@ -0,0 +1,57 @@
+package xtaieer.network.echo;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EchoClient {
+    private static final int PORT = 11880;
+    public static void main(String[] args) {
+        Socket socket = null;
+        try {
+            socket = new Socket((String)null, PORT);
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
+            List<Byte> bytes = new ArrayList<Byte>();
+            int r;
+            for(;;) {
+                bytes.clear();
+                while ((r = System.in.read()) != '\n') {
+                    outputStream.write(r);
+                }
+                outputStream.write('&');
+                outputStream.flush();
+                bytes.clear();
+                while (inputStream.available() > 0) {
+                    bytes.add((byte) inputStream.read());
+                }
+                byte[] bs = new byte[bytes.size()];
+                for (int i = 0; i < bytes.size(); i++) {
+                    bs[i] = bytes.get(i);
+                }
+                System.out.println("echo clint " + new String(bs));
+            }
+        }
+        catch(UnknownHostException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(socket != null) {
+                try {
+                    socket.close();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ EchoServer/src/main/java/xtaieer/network/echo/EchoServer.java
@@ -0,0 +1,65 @@
+package xtaieer.network.echo;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EchoServer {
+
+    private static final int PORT = 11880;
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            for(;;) {
+                Socket socket = serverSocket.accept();
+                InputStream inputStream = new BufferedInputStream(socket.getInputStream());
+                OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
+                try {
+                    int b;
+                    List<Byte> readBytes = new ArrayList<>();
+                    while ((b = inputStream.read()) != -1) {
+                        readBytes.add((byte) b);
+                        if ((char) b == '&') {
+                            for (int i = 0; i < readBytes.size(); i++) {
+                                outputStream.write(readBytes.get(i));
+                            }
+                            outputStream.flush();
+                            readBytes.clear();
+                        }
+                    }
+                }
+                catch(SocketException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(socket != null) {
+                        try {
+                            socket.close();
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
