d7a673eebc00f42758fa74d4174bdc55946a5756
xtaieer <920557080@qq.com>
2019-05-11 21:49:03 +0800

    添加退出

--- EchoClient/src/main/java/xtaieer/network/echo/EchoClient.java
+++ EchoClient/src/main/java/xtaieer/network/echo/EchoClient.java
@@ -1,11 +1,7 @@
 package xtaieer.network.echo;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,17 +12,25 @@ public class EchoClient {
         Socket socket = null;
         try {
             socket = new Socket((String) null, PORT);
-            InputStream inputStream = socket.getInputStream();
+            InputStream inputStream = new BufferedInputStream(socket.getInputStream());
             OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
             List<Byte> bytes = new ArrayList<Byte>();
             int r;
             for (;;) {
                 bytes.clear();
                 while ((r = System.in.read()) != '\n') {
+                    bytes.add((byte) r);
                     outputStream.write(r);
                 }
+                bytes.add((byte) r);
                 outputStream.write(r);
                 outputStream.flush();
+
+                byte[] readBytes = new byte[bytes.size()];
+                for(int i = 0; i < bytes.size(); i ++) {
+                    readBytes[i] = bytes.get(i);
+                }
+
                 bytes.clear();
                 while ((r = inputStream.read()) != '\n') {
                     bytes.add((byte) r);
@@ -36,8 +40,15 @@ public class EchoClient {
                 for (int i = 0; i < bytes.size(); i++) {
                     bs[i] = bytes.get(i);
                 }
-                System.out.println("echo clint " + new String(bs));
+                System.out.print(new String(bs));
+
+                if("ShutDown\n".equals(new String(readBytes))) {
+                    break;
+                }
             }
+        } catch(ConnectException e) {
+            System.out.println("连接出错");
+            e.printStackTrace();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
